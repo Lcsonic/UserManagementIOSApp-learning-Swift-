f0fe8df638425e143fb26bc1c1fcc975295d7000
Lucas Araujo <lucasaraujoss95@gmail.com>
2025-04-06 14:02:19 -0300

    Initial Commit

---
+++ AppGerenciamentoIOS/AppGerenciamentoIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppGerenciamentoIOSApp.swift
+//  AppGerenciamentoIOS
+//
+//  Created by Lucas Araujo on 06/04/25.
+//
+
+import SwiftUI
+
+@main
+struct AppGerenciamentoIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
